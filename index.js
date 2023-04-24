92729a0d4b24fe53d072e8c6af7cf5f4a5601621
Keino Chichester <keino.chichester@gmail.com>
2023-04-24 19:44:22 -0400

    firebase auth tweaks

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const cors = require("cors");
-const User = require("./config");
+const firebase = require("firebase");
+require("firebase/auth");
+const admin = require("firebase-admin");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUZQEQHDvLLmYdK9bqB-crKOFStpULkmk",
+    authDomain: "g249api.firebaseapp.com",
+    databaseURL: "https://g249api-default-rtdb.firebaseio.com",
+    projectId: "g249api",
+    storageBucket: "g249api.appspot.com",
+    messagingSenderId: "966250425627",
+    appId: "1:966250425627:web:096151ed1fd4dce5e077be",
+    measurementId: "G-QW1PG8GZ59",
+};
+
+firebase.initializeApp(firebaseConfig);
+admin.initializeApp(firebaseConfig); // Initialize Firebase Admin SDK
+
+const db = firebase.firestore();
+const User = db.collection("Users");
+const auth = firebase.auth();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Example route for user registration
+app.post("/users", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userRecord = await admin.auth().createUser({
+            // Use Firebase Admin SDK to create user
+            email: email,
+            password: password,
+        });
+        console.log("Successfully created new user:", userRecord.uid);
+        await User.add({ email: userRecord.email }); // Add user to Firestore collection
+        res.send({ msg: "User registered successfully" });
+    } catch (error) {
+        console.error("Error creating new user:", error);
+        res.status(500).send({ error: error.message });
+    }
+});
+
 app.get("/", async (req, res) => {
-    const snapshot = await User.get(); // cant use await without async
-    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })); /// id dont naturally come with the data object, the spread is to add it
+    const snapshot = await User.get();
+    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
     res.send(list);
 });
 
@@ -31,4 +70,4 @@ app.post("/delete", async (req, res) => {
     res.send({ msg: "Deleted" });
 });
 
-app.listen(4000, () => console.log("Up & Running on port 4000")); 
+app.listen(4000, () => console.log("Up & Running on port 4000"));
